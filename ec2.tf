afbb4c5ce6bdf4a614295c8721d224901dc57c63
jerrish-devops <131433115+jerrish-devops@users.noreply.github.com>
2023-11-20 21:20:17 +0530

    day10

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,23 @@
-#This resource creates an ec2 amazon linux instance
+#This resource creates an ec2 amazon linux instance in mumbai region
 resource "aws_instance" "demo_instance1" {
   ami           = "ami-0645cf88151eb2007"
   instance_type = "t2.micro" 
-  key_name      = "tfclass-mumbai-kp"             #this key exist in mumbai region only
+  key_name      = "awsclass-mumbai-kp"             #this key must exist in mumbai region
   tags = {
-    Name    = "linux-webserver"
+    Name    = "linux-webserver-mumbai"
     Project = "projecta"
   }
   subnet_id              = "subnet-0453673da9a08bfd1"
   vpc_security_group_ids = ["sg-0f17b7be16cedabbe"]
+}
+
+#This resource creates an ec2 amazon linux instance in singapore region
+resource "aws_instance" "demo_instance2" {
+  ami           = "ami-02453f5468b897e31"
+  instance_type = "t2.micro" 
+  tags = {
+    Name    = "linux-webserver-singapore"
+    Project = "projecta"
+  }
+  provider = aws.account1-singapore
 }
--- s3.tf
+++ s3.tf
@@ -1,7 +1,16 @@
-resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "jerrish-tf-class"
+resource "aws_s3_bucket" "demo_bucket1" {
+  bucket = "jerrish-tf-class-mumbai"
   force_destroy = true
   tags = {
     Name = "testing-bucket"
   }
+}
+
+resource "aws_s3_bucket" "demo_bucket2" {
+  bucket = "jerrish-tf-class-singapore12"
+  force_destroy = true
+  tags = {
+    Name = "testing-bucket"
+  }
+  provider = aws.account1-singapore
 }
