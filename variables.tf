2804118ae88524377944624ae0bf7d1228d9ffc3
jerrish-devops <131433115+jerrish-devops@users.noreply.github.com>
2023-11-29 21:09:14 +0530

    day15

--- variables.tf
+++ variables.tf
@@ -1,11 +1,22 @@
+variable "access_key" {
+  
+}
+
+variable "secret_key" {
+  
+}
+
 variable "ami" {
+  description = "This is amazon linux instance image"
   default = "ami-0645cf88151eb2007"
 }
 
 variable "instance_type" {
+  description = "This is a free tier instance type"
   default = "t2.micro"
 }
 
 variable "allow_specific" {
-  default = "3.3.3.3/32"
+  description = "This allows corp dc to access ec2 instance"
+  default = "1.1.1.1/32"
 }
